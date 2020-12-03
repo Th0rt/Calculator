15db1883454d304ce3a5ebe3b113d3ed48564109
Tsukasa Yamada <yamaxanado@gmail.com>
2020-12-03 12:49:47 +0000

    install express

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,24 @@
-const hello = (name: string): void => console.log(`Hello,${name}`);
-hello('Tsukasa');
+import express from 'express';
+
+const app: express.Express = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const router: express.Router = express.Router();
+router.get('/api/getTest', (req: express.Request, res: express.Response) => {
+  res.send(req.query);
+});
+
+router.post('api/postTest', (req: express.Request, res: express.Response) => {
+  res.send(req.body);
+});
+app.use(router);
+
+app.listen(3000, () => console.log('Example app litening on port 3000!'));
